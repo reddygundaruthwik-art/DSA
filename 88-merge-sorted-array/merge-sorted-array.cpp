7af505fad39adf80a1d2583b9ad6603818d1c998
reddygundaruthwik-art <reddygundaruthwik@gmail.com>
2026-08-03 19:48:00 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (9.55%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,32 +1,23 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-
-        vector<int> nums3(m + n);
-
-        int left = 0;
-        int right = 0;
-        int index = 0;
-
-        while (left < m && right < n) {
-            if (nums1[left] <= nums2[right]) {
-                nums3[index++] = nums1[left++];
+        int left = m-1;
+        int right =0;
+        while(left >= 0 && right <n){
+            if (nums1[left] > nums2[right]){
+                swap(nums1[left], nums2[right]);
+                left--;
+                right++;
             }
             else {
-                nums3[index++] = nums2[right++];
+                break;
             }
         }
+        sort(nums1.begin(), nums1.begin() +m);
+        sort(nums2.begin(), nums2.end());
 
-        while (left < m) {
-            nums3[index++] = nums1[left++];
-        }
-
-        while (right < n) {
-            nums3[index++] = nums2[right++];
-        }
-
-        for (int i = 0; i < m + n; i++) {
-            nums1[i] = nums3[i];
+        for( int i =0; i<n; i++){
+            nums1[m+i] = nums2[i];
         }
     }
 };

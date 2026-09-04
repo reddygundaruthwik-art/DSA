daa97e8db322730937530ea117e9ec89f68c194f
reddygundaruthwik-art <reddygundaruthwik@gmail.com>
2026-09-04 15:27:32 +0530

    Time: 0 ms (100.00%) | Memory: 95 MB (94.71%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,56 +1,54 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        
-        // Always binary search on the smaller array
-        if (nums1.size() > nums2.size()) {
-            return findMedianSortedArrays(nums2, nums1);
-        }
-
         int n1 = nums1.size();
         int n2 = nums2.size();
+
+        if(n1 > n2){
+            return findMedianSortedArrays(nums2, nums1);
+        }
 
         int low = 0;
         int high = n1;
 
-        int total = n1 + n2;
-        int leftSize = (total + 1) / 2;
+        int left = (n1 + n2 + 1)/2;
 
-        while (low <= high) {
+        int n = n1 + n2;
 
-            int cut1 = (low + high) / 2;
-            int cut2 = leftSize - cut1;
+        while(low <= high){
+            int mid1= low + (high - low)/2;
 
-            // Elements just outside the partition
-            int l1 = (cut1 == 0) ? INT_MIN : nums1[cut1 - 1];
-            int r1 = (cut1 == n1) ? INT_MAX : nums1[cut1];
+            int mid2 = left - mid1;
 
-            int l2 = (cut2 == 0) ? INT_MIN : nums2[cut2 - 1];
-            int r2 = (cut2 == n2) ? INT_MAX : nums2[cut2];
 
-            // Correct partition
-            if (l1 <= r2 && l2 <= r1) {
+            int l1 = INT_MIN; int l2 = INT_MIN;
 
-                // Odd total number of elements
-                if (total % 2 == 1) {
+            int r1 = INT_MAX; int r2 = INT_MAX;
+
+            if(mid1 < n1) r1 = nums1[mid1];
+            if(mid2 < n2) r2 = nums2[mid2];
+
+            if(mid1 - 1 >= 0) l1 = nums1[mid1 -1];
+            if(mid2 - 1 >= 0) l2 = nums2[mid2 -1];
+
+            if(l1 <= r2 && l2 <= r1){
+                if(n % 2 == 1){
                     return max(l1, l2);
                 }
 
-                // Even total number of elements
-                return (max(l1, l2) + min(r1, r2)) / 2.0;
+                else{
+                    return (double)(max(l1, l2) + min(r1, r2)) / 2.0;
+                }
             }
 
-            // We have taken too many elements from nums1
-            else if (l1 > r2) {
-                high = cut1 - 1;
+            else if(l1 > r2){
+                high = mid1 -1;
             }
 
-            // We need to take more elements from nums1
-            else {
-                low = cut1 + 1;
+            else{
+                low = mid1 + 1;
             }
         }
-
         return 0.0;
     }
 };

3f25241dd2b0e78201804b71631737208410bd05
reddygundaruthwik-art <reddygundaruthwik@gmail.com>
2026-09-25 20:57:22 +0530

    Time: 14 ms (32.14%) | Memory: 17.5 MB (30.36%) - LeetSync

---
+++ 1188-brace-expansion-ii/brace-expansion-ii.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+
+    set<string> parseUnion(string &s, int &i) {
+        set<string> result = parseConcat(s, i);
+
+        while (i < s.size() && s[i] == ',') {
+            i++;  // skip ','
+
+            set<string> next = parseConcat(s, i);
+
+            result.insert(next.begin(), next.end());
+        }
+
+        return result;
+    }
+
+    set<string> parseConcat(string &s, int &i) {
+        set<string> result;
+        result.insert("");
+
+        while (i < s.size() && s[i] != '}' && s[i] != ',') {
+
+            set<string> cur;
+
+            // Letter
+            if (s[i] >= 'a' && s[i] <= 'z') {
+                cur.insert(string(1, s[i]));
+                i++;
+            }
+
+            // Braces
+            else if (s[i] == '{') {
+                i++;  // skip '{'
+
+                cur = parseUnion(s, i);
+
+                i++;  // skip '}'
+            }
+
+            // Concatenate
+            set<string> temp;
+
+            for (string a : result) {
+                for (string b : cur) {
+                    temp.insert(a + b);
+                }
+            }
+
+            result = temp;
+        }
+
+        return result;
+    }
+
+    vector<string> braceExpansionII(string expression) {
+
+        int i = 0;
+
+        set<string> ans = parseUnion(expression, i);
+
+        return vector<string>(ans.begin(), ans.end());
+    }
+};
